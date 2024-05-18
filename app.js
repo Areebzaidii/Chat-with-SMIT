ffda663e466ea2299506717ffcc732b215539f9e
areebzaidii <153609791+Areebzaidii@users.noreply.github.com>
2024-05-18 09:31:01 -0700

    all

---
+++ app.js
@@ -0,0 +1,98 @@
+
+
+function abc(){
+    let userInput = document.getElementById("userText").value.toLowerCase();
+    (document.getElementById("ali").innerHTML+="<h2>"+ document.getElementById("userText").value +"</h2>")
+    if(userInput.toLowerCase()==="hi"||userInput.toLowerCase()==="hello"||userInput.toLowerCase()==="helo"||userInput.toLowerCase()==="hy"){
+        document.getElementById("userText").value=""
+        document.getElementById("userText").placeholder="Type..."
+        setTimeout(function(){
+            document.getElementById("ali").innerHTML+="<li>"+"hello chat with saylani im information hub.what can i help you?"+"</li>"       
+
+        },2000)
+        
+        setTimeout(function(){
+            document.getElementById("ali").innerHTML+="<li>"+"what kind of information do you need"+"</li>"       
+            
+        },4000)
+         
+        setTimeout(function(){
+            document.getElementById("ali").innerHTML+=
+            "<ol>"+"<b>"+
+            "<li>"+"" +"About Education"+"</li>"+    
+            "<li>"+"How is Molana Basheer farooqi"+"</li>"+
+            "<li>"+"Blood Bank / Thalassemia Treatment"+"</li>"+
+            "<li>"+"Medical"+"</li>"+
+            "<li>"+"Food"+"</li>"+
+            "</ol>"
+
+            
+            document.getElementById("userText").placeholder=""
+        },6000)
+    }
+   else if(userInput==="1"||userInput.toLowerCase()==="education"||userInput.toLowerCase()==="about education"){
+        document.getElementById("userText").value=""
+        document.getElementById("userText").placeholder="Type..."
+        setTimeout(function(){
+            document.getElementById("ali").innerHTML+="<li>"+"Services Education Technical Education SMIT"+"</li>"
+            +"<b>"+"SMIT (Saylani IT ) "+"</b>" 
+            +"<p>"+"Today is the age of information technology and we aim to equip our youth with computer programming skills and prepare them for the development of the country. In this regard, our Saylani Mass IT Training Program providing vocational training to more than 75,000 students in Web and Mobile Application Development, CCNA, Graphics Designing, Computer Based Accounting in Karachi, Hyderabad, Faisalabad, Islamabad. Our goal is to develop more than 1 million software developers across the country, which will add about $100 billion annually to Pakistan's economy"
+            +"</p>"   
+            document.getElementById("userText").placeholder=""
+
+        },4000)
+    }
+    
+    else if(userInput==="2"||userInput.toLowerCase()==="maulana bashir farooqi"||userInput.toLowerCase()==="bashir farooqi"||userInput.toLowerCase()==="chairman"||userInput.toLowerCase()==="saylani chairman"||userInput.toLowerCase()==="how is saylani chairman"){
+        document.getElementById("userText").value=""
+        document.getElementById("userText").placeholder="Type..."
+        setTimeout(function(){
+            document.getElementById("ali").innerHTML+="<li>"+"Maulana Bashir Farooqi Qadri"+"</li>"+"<p>"+"is the Chairman of Saylani Welfare Trust, which is one of the biggest welfare organizations in Pakistan. But do you know the biography of the man who is behind this whole work and progress"+
+            "</p>"
+            +"<p>"+"In recent years Saylani Welfare Trust has steadily grown into one of the most active and renowned welfare organizations in the country. But many people are unaware from the life and profile of the Maulana Bashir who established the organization to help the people of Pakistan"
+            +"</p>"
+            +"<p>"+"Bashir Ahmed Polani was born on March 3, 1963 in the month of Ramazan in the city of Shikarpur, Sindh. He belongs to a noble and religious family from the Bantawa Community and his father Abdul Ghaffar Polani was a successful businessmen."
+            +"</p>"
+            +"<p>"+"He completed his schooling from Islamia High School Shikarpur and then travelled to Karachi in 1975 to pursue college & Islamic studies. In Karachi he got admission in Abdullah Haroon College from where he completed his intermediate education.3"
+            +"</p>" 
+            +"<p>"+"After completing college he pursued his Islamic studies from Darul-ul-Uloom Amjadia from where he successfully did his Dars-e-Nizami"
+            +"</p>"
+            document.getElementById("userText").placeholder=""
+
+        },7000)
+    }
+    else{
+    document.getElementById("userText").value=""
+        document.getElementById("userText").placeholder="Type..."
+        setTimeout(function(){
+            document.getElementById("ali").innerHTML+="<li>"+"Soryy "+"</li>"+"<p>"+"i am currently not updated for your all question inshallah i will answer your all questions As soon As possible i update"+
+            "</p>"
+            document.getElementById("userText").placeholder=""
+
+        },2000)
+
+    }
+    
+
+   
+   
+}
+   
+// function abc(){
+//     if(document.getElementById("userText").value==="hi"){
+    // document.getElementById("me").innerHTML=document.getElementById("userText").value;
+    // document.getElementById("chtai").innerHTML="helo";
+// document.getElementById("content").style.display="block";
+// }
+// else if(document.getElementById("userText").value==="how"){
+    //     document.getElementById("me").innerHTML=document.getElementById("userText").value;
+    //     document.getElementById("chtai").innerHTML="helo";
+    //     document.getElementById("content").style.display="block";
+    //     }
+    
+    // }
+    console.log(
+        )
+        function abd() {
+            
+        }
